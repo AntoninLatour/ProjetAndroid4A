beb5fe68c999d01e22ddd61b7e74c335fdad7d8f
Antonin Latour <antonin.latour@gmail.com>
2019-12-23 03:12:19 +0100

    Fragments et navigation

---
+++ app/src/main/java/com/example/android_4a/ui/settings/SettingsFragment.kt
@@ -0,0 +1,5 @@
+package com.example.android_4a.ui.settings
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment(){}
---
+++ app/src/main/java/com/example/android_4a/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.android_4a.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
